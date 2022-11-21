9f09f6e3228dadaa57c7a1cdf6d690e78a89632a
rafal19987 <rafal987.1@o2.pl>
2022-11-21 21:35:40 +0100

    Add animated hamburger button

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,8 @@
 console.log('connected');
+const hamburgerMenu = document.querySelector('.hamburgerBtn');
+
+hamburgerMenu.addEventListener('click', () => {
+  hamburgerMenu.classList.contains('open')
+    ? hamburgerMenu.classList.remove('open')
+    : hamburgerMenu.classList.add('open');
+});
